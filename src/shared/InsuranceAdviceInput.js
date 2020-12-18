f7aefb27143ce97d121a237de2a1c22c2c76b285
rennanbadaro <rennanrb@icloud.com>
2020-12-18 12:56:53 -0300

    feat: create InsuranceAdviceInput model

---
+++ src/shared/InsuranceAdviceInput.js
@@ -0,0 +1,19 @@
+class InsuranceAdviceInput {
+  constructor({
+    age,
+    dependents,
+    house,
+    income,
+    maritalStatus,
+    riskQuestions,
+    vehicle,
+  }) {
+    this.age = age;
+    this.dependents = dependents;
+    this.house = house;
+    this.income = income;
+    this.maritalStatus = maritalStatus;
+    this.riskQuestions = riskQuestions;
+    this.vehicle = vehicle;
+  }
+}
