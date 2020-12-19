c9eda0f5828378048fb209720a482ef2309a9a78
rennanbadaro <rennanrb@icloud.com>
2020-12-18 22:49:31 -0300

    feat(scores): create use case GenerateScore

---
+++ src/domain/score/use-cases/GenerateScore.js
@@ -0,0 +1,16 @@
+const ProfilerChainFactory = require('../factories/ProfilerChainFactory');
+
+class GenerateScore {
+  constructor(params) {
+    this.ProfilerChainFactory =
+      params.ProfilerChainFactory || ProfilerChainFactory;
+  }
+
+  run(adviceInput) {
+    const profilerChain = this.ProfilerChainFactory.getChain();
+
+    return profilerChain.run(adviceInput);
+  }
+}
+
+module.exports = GenerateScore;
