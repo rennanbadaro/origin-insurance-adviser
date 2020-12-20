157bdc9e14d90cf838dccfb4ea4d033a32488c1a
rennanbadaro <rennanrb@icloud.com>
2020-12-20 00:32:53 -0300

    feat(app): add controller method to insurance-advice route

--- src/app/routers/insurance-advice.js
+++ src/app/routers/insurance-advice.js
@@ -1,12 +1,23 @@
 const Router = require('@koa/router');
 
 const { postSchema } = require('../schemas/insurance-advice');
-const getSchemaValidatorMiddleware = require('../middlewares/get-schema-validator');
+const schemaValidator = require('../middlewares/schema-validator');
+
+const dependencyContainer = require('../../shared/dependency-container/container');
+const dependenciesEnum = require('../../shared/dependency-container/dependencies.enum');
+
+const controller = dependencyContainer.get(
+  dependenciesEnum.INSURANCE_ADVICE_CONTROLLER
+);
 
 const router = new Router({
   prefix: '/insurance-advice',
 });
 
-router.post('/', getSchemaValidatorMiddleware(postSchema));
+router.post(
+  '/',
+  schemaValidator(postSchema),
+  (...args) => controller.generateInsuranceAdvice(...args),
+);
 
 module.exports = router;
